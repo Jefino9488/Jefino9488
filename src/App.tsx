491f61e749f7a25727bce53fcef4439bc7cb93c3
Jefino9488 <jefinojacob9488@gmail.com>
2025-04-20 23:37:12 +0530

    refactor About component to enhance layout and add interactive tabs for overview, timeline, and skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,100 @@
-import { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Home from './components/Home';
-import BlogList from './components/BlogList';
-import BlogPost from './components/BlogPost';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import TermsOfService from './components/TermsOfService';
-import About from './components/About';
-import Projects from './components/Projects';
-import Loader from './components/Loader';
-import TransitionWrapper from './components/TransitionWrapper';
-import { ProjectsProvider } from './components/ProjectsContext';
-import { useSmoothScroll } from './hooks/useSmoothScroll';
+import { Suspense } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
+import Home from "./components/Home"
+import BlogList from "./components/BlogList"
+import BlogPost from "./components/BlogPost"
+import PrivacyPolicy from "./components/PrivacyPolicy"
+import TermsOfService from "./components/TermsOfService"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Certificates from "./components/Certificates"
+import Loader from "./components/Loader"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import TransitionWrapper from "./components/TransitionWrapper"
+import { ProjectsProvider } from "./components/ProjectsContext"
+import { useSmoothScroll } from "./hooks/useSmoothScroll"
 
 function AppContent() {
-    useSmoothScroll();
+    useSmoothScroll()
 
     return (
-        <AnimatePresence mode="wait">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <TransitionWrapper>
-                            <Home />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/blog"
-                    element={
-                        <TransitionWrapper>
-                            <BlogList />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/blog/:id"
-                    element={
-                        <TransitionWrapper>
-                            <BlogPost />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/privacy-policy"
-                    element={
-                        <TransitionWrapper>
-                            <PrivacyPolicy />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/terms-of-service"
-                    element={
-                        <TransitionWrapper>
-                            <TermsOfService />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/about"
-                    element={
-                        <TransitionWrapper>
-                            <About />
-                        </TransitionWrapper>
-                    }
-                />
-                <Route
-                    path="/projects"
-                    element={
-                        <TransitionWrapper>
-                            <Projects />
-                        </TransitionWrapper>
-                    }
-                />
-            </Routes>
-        </AnimatePresence>
-    );
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow pt-16">
+                <AnimatePresence mode="wait">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <TransitionWrapper>
+                                    <Home />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/blog"
+                            element={
+                                <TransitionWrapper>
+                                    <BlogList />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/blog/:id"
+                            element={
+                                <TransitionWrapper>
+                                    <BlogPost />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/privacy-policy"
+                            element={
+                                <TransitionWrapper>
+                                    <PrivacyPolicy />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/terms-of-service"
+                            element={
+                                <TransitionWrapper>
+                                    <TermsOfService />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/about"
+                            element={
+                                <TransitionWrapper>
+                                    <About />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/projects"
+                            element={
+                                <TransitionWrapper>
+                                    <Projects />
+                                </TransitionWrapper>
+                            }
+                        />
+                        <Route
+                            path="/certificates"
+                            element={
+                                <TransitionWrapper>
+                                    <Certificates />
+                                </TransitionWrapper>
+                            }
+                        />
+                    </Routes>
+                </AnimatePresence>
+            </main>
+            <Footer />
+        </div>
+    )
 }
 
 function App() {
@@ -89,7 +106,7 @@ function App() {
                 </Suspense>
             </ProjectsProvider>
         </Router>
-    );
+    )
 }
 
-export default App;
+export default App
